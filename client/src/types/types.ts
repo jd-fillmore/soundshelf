3af2b1ae52511d6d4e2c42041e4cd425bd9ecdc7
JD Fillmore <jd@infused.agency>
2023-04-17 17:46:16 -0400

    adjust albumData types

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,8 +1,9 @@
 export type AlbumData = {
+  id: number;
   album_image: string;
   album_name: string;
   band_name: string;
   genre: string;
   status: string;
-  year_listened: string;
+  year_listened: number;
 };
