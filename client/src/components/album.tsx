28a7b79fbe8188959939f57424311df7b8937043
JD Fillmore <jd@infused.agency>
2023-04-17 17:47:39 -0400

    add edit/del button, add styles to album component

--- client/src/components/album.tsx
+++ client/src/components/album.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Container, Flex, Card, Button } from "@chakra-ui/react";
 import { AlbumData } from "../types/types";
 
 interface AlbumProps {
@@ -8,18 +9,26 @@ interface AlbumProps {
 const Album: FC<AlbumProps> = ({ data }) => {
   return (
     <>
-      {data.map(({ album_image, album_name, band_name, genre, status, year_listened }) => (
-        <div key={`${album_name}-${band_name}`}>
-          <img src={album_image} alt={album_name} />
-          <h2>{band_name}</h2>
-          <p>
-            {album_name} | {genre}
-          </p>
-          <em>
-            {status} | {year_listened}
-          </em>
-        </div>
-      ))}
+      <Container maxW="1440px">
+        <Flex direction="row" flexWrap="wrap">
+          {data.map(({ id, album_image, album_name, band_name, genre, status, year_listened }) => (
+            <Flex key={id} width={{ base: "100%", md: "50%", lg: "33.33%" }}>
+              <Card key={id}>
+                <img src={album_image} alt={album_name} />
+                <h2>{band_name}</h2>
+                <p>
+                  {album_name} | {genre}
+                </p>
+                <em>
+                  {status} | {year_listened}
+                </em>
+                <Button colorScheme="yellow">Edit</Button>
+                <Button colorScheme="red">Delete</Button>
+              </Card>
+            </Flex>
+          ))}
+        </Flex>
+      </Container>
     </>
   );
 };
