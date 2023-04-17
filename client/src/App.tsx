511176bc6486e5569a4ee5b8b3b409d0d67c5e63
JD Fillmore <jd@infused.agency>
2023-04-17 17:48:12 -0400

    add hero and nav to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import Hero from "./components/shared/hero";
 import AlbumList from "./components/albumList";
+import Nav from "./components/shared/nav";
 
 function App() {
   return (
     <>
+      <Nav />
+      <Hero />
       <AlbumList />
     </>
   );
