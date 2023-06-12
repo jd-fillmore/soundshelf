be3a4317563bbc6c3a86fc468df37573df7cf6eb
JD Fillmore <jd@infused.agency>
2023-06-11 20:56:16 -0400

    Add testing libraries

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import React from "react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), babel()],
+});
